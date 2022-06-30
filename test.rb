150026d3fcc547d18c43bf52f740ed63d3ba4f4d
nissyi <yuta.onishi@pepabo.com>
2022-06-30 16:25:01 +0900

    有効なメモを投稿した場合のテストを追加
    リダイレクト、投稿内容が表示されているかのテストを追加しました。

--- test.rb
+++ test.rb
@@ -20,4 +20,16 @@ class AppTest < Test::Unit::TestCase
     get '/hogehoge'
     assert_equal 'ページが見つかりませんでした。', last_response.body
   end
+
+  def test_post_valid_memo
+    title = 'test_title'
+    content = 'example_text'
+
+    post '/memos', { title: title, content: content }
+    follow_redirect!
+    assert last_response.ok?
+    assert_equal 'http://example.org/', last_request.url
+    assert_includes title, last_response.body
+    assert_includes content, last_response.body
+  end
 end
