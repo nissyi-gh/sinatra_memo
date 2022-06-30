daea22d22b99bff631584e17fb317ee4c86b83a5
nissyi <yuta.onishi@pepabo.com>
2022-06-30 19:06:30 +0900

    Memoインスタンスを作成、参照できるようにしました。
    メモをクラスのインスタンスの集まりだと考えたため、インスタンスを参照できるようにしました。

--- memo.rb
+++ memo.rb
@@ -2,10 +2,25 @@ class Memo
   attr_reader :id
   attr_accessor :title, :content, :created_at
 
+  @@file = File.open('./memos.json', 'a')
+  @@instances = []
+
   def initialize(id, title, content = nil, created_at)
     @id = id
     @title = title
     @content = content
     @created_at = created_at
+  end
+
+  def self.all
+    @@instances
+  end
+
+  def create
+    @@instances << self
+  end
+
+  def save
+    # ファイルへの保存処理を書く
   end
 end
