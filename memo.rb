1aee51d3edfedb317adb49e478c96e90f998cce8
nissyi <yuta.onishi@pepabo.com>
2022-07-03 16:14:21 +0900

    RuboCop対応
    DRY化、クラス変数のクラスインスタンス変数化などを中心に対応しました。

--- memo.rb
+++ memo.rb
@@ -1,9 +1,11 @@
+# frozen_string_literal: true
+
 class Memo
   attr_reader :id
   attr_accessor :title, :content, :created_at, :deleted_at
 
-  @@instances = []
-  @@JSON_FILE = './.memos.json'
+  @instances = []
+  JSON_FILE = './.memos.json'
 
   def initialize(id, title, content, created_at, deleted_at = nil)
     @id = id
@@ -15,54 +17,54 @@ class Memo
 
   class << self
     def all
-      @@instances
+      @instances
     end
 
     def all_ignore_deleted
-      @@instances.filter(&:delete?)
+      @instances.filter(&:delete?)
     end
 
     def new_id
-      @@instances.size + 1
+      @instances.size + 1
     end
 
     def clear
-      @@instances.clear
+      @instances.clear
     end
 
     def create(title:, content: nil)
       return if title.empty?
 
-      memo = Memo.new(self.new_id, title, content, DateTime.now)
-      @@instances << memo
+      memo = Memo.new(new_id, title, content, DateTime.now)
+      @instances << memo
       save
     end
 
     def delete(memo_id)
-      @@instances.each { |memo| memo.deleted_at = DateTime.now if memo.id == memo_id.to_i }
+      @instances.each { |memo| memo.deleted_at = DateTime.now if memo.id == memo_id.to_i }
       save
     end
 
     def find(memo_id)
-      @@instances.find { |memo| memo.id == memo_id.to_i }
+      @instances.find { |memo| memo.id == memo_id.to_i }
     end
 
     def save
       return if ENV['APP_ENV'] == 'test'
 
       memos_hash = {}
-      @@instances.each { |memo| memos_hash["memo_#{memo.id}".to_sym] = memo.to_h }
-      File.open(@@JSON_FILE, 'w') { |file| file.write(memos_hash.to_json) }
+      @instances.each { |memo| memos_hash["memo_#{memo.id}".to_sym] = memo.to_h }
+      File.open(JSON_FILE, 'w') { |file| file.write(memos_hash.to_json) }
     end
 
     def load
-      return if File.empty?(@@JSON_FILE) || !File.exist?(@@JSON_FILE)
+      return if File.empty?(JSON_FILE) || !File.exist?(JSON_FILE)
 
       memos_json = {}
-      File.open(@@JSON_FILE, 'r') { |file| memos_json = JSON.parse(file.readline, symbolize_names: true)}
+      File.open(JSON_FILE, 'r') { |file| memos_json = JSON.parse(file.readline, symbolize_names: true) }
 
       memos_json.each_value do |memo|
-        @@instances << Memo.new(
+        @instances << Memo.new(
           memo[:id],
           memo[:title],
           memo[:content],
--- test.rb
+++ test.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 ENV['APP_ENV'] = 'test'
 
 require_relative './app'
@@ -14,6 +16,9 @@ class AppTest < Test::Unit::TestCase
 
   def setup
     Memo.clear
+    @title = 'test_title'
+    @content = 'example_text'
+    @edit = 'edit'
   end
 
   def test_home_response
@@ -27,116 +32,93 @@ class AppTest < Test::Unit::TestCase
   end
 
   def test_post_valid_memo
-    title = 'test_title'
-    content = 'example_text'
-
-    post '/memos', { title: title, content: content }
+    post '/memos', { title: @title, content: @content }
     follow_redirect!
     assert last_response.ok?
     assert_equal TEST_HOST, last_request.url
-    assert_includes last_response.body, title
-    assert_includes last_response.body, content
+    assert_includes last_response.body, @title
+    assert_includes last_response.body, @content
   end
 
   def test_post_valid_memo_not_include_content
-    title = 'test_title'
-    content = nil
+    @content = nil
 
-    post '/memos', { title: title, content: content }
+    post '/memos', { title: @title, content: @content }
     follow_redirect!
     assert last_response.ok?
     assert_equal TEST_HOST, last_request.url
-    assert_includes last_response.body, title
+    assert_includes last_response.body, @title
   end
 
   def test_post_invalid_memo_because_not_include_title
-    title = ''
-    content = 'example_text'
+    @title = ''
 
-    post '/memos', { title: title, content: content }
+    post '/memos', { title: @title, content: @content }
     follow_redirect!
     assert_equal TEST_HOST, last_request.url
     assert_includes last_response.body, 'タイトルが入力されていません。'
   end
 
   def test_delete_memo
-    title = 'test_title'
-    content = 'example_text'
-
-    post '/memos', { title: title, content: content }
+    post '/memos', { title: @title, content: @content }
     follow_redirect!
-    assert_includes last_response.body, title
-    assert_includes last_response.body, content
+    assert_includes last_response.body, @title
+    assert_includes last_response.body, @content
 
-    delete "/memos/1"
+    delete '/memos/1'
     follow_redirect!
-    assert_not_includes last_response.body, title
-    assert_not_includes last_response.body, content
+    assert_not_includes last_response.body, @title
+    assert_not_includes last_response.body, @content
   end
 
   def test_edit_memo_title_and_content
-    title = 'test_title'
-    content = 'example_text'
-    edit_text = '_edit!!!'
-
-    post '/memos', { title: title, content: content }
+    post '/memos', { title: @title, content: @content }
     follow_redirect!
-    assert_includes last_response.body, title
-    assert_includes last_response.body, content
+    assert_includes last_response.body, @title
+    assert_includes last_response.body, @content
 
-    patch "/memos/1", { title: title + edit_text, content: content + edit_text }
+    patch '/memos/1', { title: @title + @edit, content: @content + @edit }
     follow_redirect!
-    assert_includes last_response.body, title + edit_text
-    assert_includes last_response.body, content + edit_text
+    assert_includes last_response.body, @title + @edit
+    assert_includes last_response.body, @content + @edit
   end
 
   def test_edit_only_memo_title
-    title = 'test_title'
-    content = 'example_text'
-    edit_text = '_edit!!!'
-
-    post '/memos', { title: title, content: content }
+    post '/memos', { title: @title, content: @content }
     follow_redirect!
-    assert_includes last_response.body, title
-    assert_includes last_response.body, content
+    assert_includes last_response.body, @title
+    assert_includes last_response.body, @content
 
-    patch "/memos/1", { title: title + edit_text, content: content }
+    patch '/memos/1', { title: @title + @edit, content: @content }
     follow_redirect!
-    assert_includes last_response.body, title + edit_text
-    assert_includes last_response.body, content
+    assert_includes last_response.body, @title + @edit
+    assert_includes last_response.body, @content
   end
 
   def test_edit_only_memo_content
-    title = 'test_title'
-    content = 'example_text'
-    edit_text = '_edit!!!'
-
-    post '/memos', { title: title, content: content }
+    post '/memos', { title: @title, content: @content }
     follow_redirect!
-    assert_includes last_response.body, title
-    assert_includes last_response.body, content
+    assert_includes last_response.body, @title
+    assert_includes last_response.body, @content
 
-    patch "/memos/1", { title: title, content: content + edit_text }
+    patch '/memos/1', { title: @title, content: @content + @edit }
     follow_redirect!
-    assert_includes last_response.body, title
-    assert_includes last_response.body, content + edit_text
+    assert_includes last_response.body, @title
+    assert_includes last_response.body, @content + @edit
   end
 
   def test_fail_edit_because_nothing_title
-    title = 'test_title'
-    content = 'example_text'
-
-    post '/memos', { title: title, content: content }
+    post '/memos', { title: @title, content: @content }
     follow_redirect!
-    assert_includes last_response.body, title
-    assert_includes last_response.body, content
+    assert_includes last_response.body, @title
+    assert_includes last_response.body, @content
 
     get '/memos/1'
-    patch "/memos/1", { title: '', content: content }
+    patch '/memos/1', { title: '', content: @content }
     follow_redirect!
     assert last_response.ok?
     assert_equal "#{TEST_HOST}memos/1", last_request.url.encode('UTF-8')
-    assert_includes last_response.body, title
-    assert_includes last_response.body, content
+    assert_includes last_response.body, @title
+    assert_includes last_response.body, @content
   end
 end
