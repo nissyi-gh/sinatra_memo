10a2d41aaa91f57e0e8c1f4c0ab1602093eadf51
nissyi <yuta.onishi@pepabo.com>
2022-07-04 13:55:15 +0900

    エラーメッセージの定数化とクリア処理の修正
    リダイレクト前と後で違うページを見ているときはエラーをクリアするようにしました。

--- app.rb
+++ app.rb
@@ -7,11 +7,13 @@ require_relative './memo'
 class App < Sinatra::Application
   include ERB::Util
   error = nil
+  ERROR_MESSAGE_WITHOUT_TITLE = 'タイトルが入力されていません。'
+
   Memo.load
 
   get '/' do
     @memos = Memo.all_ignore_deleted
-    @error = error
+    @error = request.url == request.referer ? error : nil
     erb :index
   end
 
@@ -21,13 +23,14 @@ class App < Sinatra::Application
 
   post '/memos' do
     memo = Memo.create(title: html_escape(params[:title]), content: html_escape(params[:content]))
-    error = memo ? nil : 'タイトルが入力されていません。'
+    error = memo ? nil : ERROR_MESSAGE_WITHOUT_TITLE
 
     redirect to('/')
   end
 
   get '/memos/:memo_id' do
     @memo = Memo.find(params[:memo_id])
+    @error = request.url == request.referer ? error : nil
     erb :edit
   end
 
@@ -38,11 +41,12 @@ class App < Sinatra::Application
 
   patch '/memos/:memo_id' do
     if params[:title].empty?
+      error = ERROR_MESSAGE_WITHOUT_TITLE
       redirect to("/memos/#{params[:memo_id]}")
     else
       memo = Memo.find(params[:memo_id])
       memo.patch(title: html_escape(params[:title]), content: html_escape(params[:content]))
-      redirect to('.')
+      redirect to('/')
     end
   end
 
