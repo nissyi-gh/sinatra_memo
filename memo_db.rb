281142978acbd57eac373cc96b2740b706d63eef
nissyi <yuta.onishi@pepabo.com>
2022-07-06 14:24:29 +0900

    CREATE TABLE文を記述
    memosテーブルを作成するためにSQLを記述しました。

--- memo_db.rb
+++ memo_db.rb
@@ -3,4 +3,16 @@
 require 'pg'
 
 module MemoDb
+  MEMOS_TABLE_NAME = 'memos'
+  connect = PG::Connection(dbname: 'postgres')
+
+  def create_table
+    connect.exec("CLEATE TABLE #{MEMOS_TABLE_NAME}(
+      id INTEGER,
+      title TEXT,
+      content TEXT,
+      created_at TIMESTAMP,
+      deleted_at TIMESTAMP;)"
+    )
+  end
 end
