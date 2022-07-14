13bc19b3aec939af5674f9c8c2ff480996ed1f49
nissyi <yuta.onishi@pepabo.com>
2022-07-14 11:23:21 +0900

    SQL文のMEMOSテーブル名の定数を削除
    定数を埋め込むのではなくSQLにベタ書きするように変更しました。

--- memo_db.rb
+++ memo_db.rb
@@ -3,11 +3,10 @@
 require 'pg'
 
 module MemoDb
-  MEMOS_TABLE_NAME = 'memos'
   CONNECT = PG::Connection.open(dbname: 'postgres')
 
   def self.create_table
-    CONNECT.exec("CREATE TABLE #{MEMOS_TABLE_NAME}(
+    CONNECT.exec("CREATE TABLE memos(
       id SERIAL,
       title TEXT NOT NULL,
       content TEXT,
@@ -16,17 +15,17 @@ module MemoDb
   end
 
   def self.load
-    CONNECT.exec("SELECT * FROM #{MEMOS_TABLE_NAME};")
+    CONNECT.exec("SELECT * FROM memos;")
   rescue StandardError
     create_table
   end
 
   def self.delete(memo_id)
-    CONNECT.exec("UPDATE #{MEMOS_TABLE_NAME} SET deleted_at = now() WHERE id = #{memo_id}")
+    CONNECT.exec("UPDATE memos SET deleted_at = now() WHERE id = #{memo_id}")
   end
 
   def self.create(memo)
-    CONNECT.exec("INSERT INTO #{MEMOS_TABLE_NAME}(title, content, created_at, deleted_at) VALUES (
+    CONNECT.exec("INSERT INTO memos(title, content, created_at, deleted_at) VALUES (
       '#{memo.title}',
       '#{memo.content}',
       '#{memo.created_at}',
@@ -35,10 +34,10 @@ module MemoDb
   end
 
   def self.update(memo_id, title, content)
-    CONNECT.exec("UPDATE #{MEMOS_TABLE_NAME} SET title = '#{title}', content = '#{content}' WHERE id = #{memo_id};")
+    CONNECT.exec("UPDATE memos SET title = '#{title}', content = '#{content}' WHERE id = #{memo_id};")
   end
 
   def self.delete_test_case
-    CONNECT.exec("DELETE FROM #{MEMOS_TABLE_NAME} WHERE title = 'test_title';")
+    CONNECT.exec("DELETE FROM memos WHERE title = 'test_title';")
   end
 end
